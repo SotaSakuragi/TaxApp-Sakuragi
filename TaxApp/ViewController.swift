4efe5cea9c9512314f5ead38c070d5a0f39d10bb
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-04 13:23:08 +0900

    Initial Commit

---
+++ TaxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxApp
+//
+//  Created by 櫻木颯大 on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
