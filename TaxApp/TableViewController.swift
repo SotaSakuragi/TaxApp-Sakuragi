e51a412ab821592a3ac792d5ee19d8088abd0626
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-04 19:26:00 +0900

    わからん

---
+++ TaxApp/TableViewController.swift
@@ -0,0 +1,135 @@
+//
+//  TableViewController.swift
+//  TaxApp
+//
+//  Created by 櫻木颯大 on 2023/09/04.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var calculationField: UITextField!
+    var numbers: [Int] = []
+    
+    func textFieldInt() -> Int? {
+        if let text = calculationField.text, let intValue = Int(text) {
+            // テキストが整数
+            return intValue
+        } else {
+            showAlert(title: "エラー!", message: "数字を入力してください")
+            return nil
+        }
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    
+    @IBAction func tex(_ sender: Any) {
+        if let intValue = textFieldInt() {
+            switch (sender as AnyObject).selectedSegmentIndex {
+            case 0:
+                let result = Double(intValue) * 1.1
+                resultLabel.text = String(result)
+            case 1:
+                let result = Double(intValue) * 1.08
+                resultLabel.text = String(result)
+            default:
+                break
+            }
+        }
+        
+    }
+    @IBAction func setButton(_ sender: Any) {
+        if let resultText = resultLabel.text, let resultValue = Double(resultText) {
+            numbers.append(Int(resultValue))
+            print(numbers)
+            let userDefaults = UserDefaults.standard
+                    userDefaults.set(numbers, forKey: "add")
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) // 表示作成
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)           // ボタン作成
+        alert.addAction(alertAction)                                                          // 表示にボタンを追加
+        present(alert, animated: true)
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return numbers.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let number = numbers[indexPath.row]
+        cell.textLabel?.text = "\(number)"
+        print(cell)
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TaxApp/ViewController.swift
+++ TaxApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var numbers: [Int] = []
 
+    @IBOutlet weak var additionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let userDefaults = UserDefaults.standard
+        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        if userDefaults.object(forKey: "add") != nil {
+            numbers = userDefaults.object(forKey: "add") as! [Int]
+            print(numbers)
+            let sum = numbers.reduce(0, +)
+            print("合計: \(sum)")
+            additionLabel.text = "合計: \(sum)"
+            // Do any additional setup after loading the view.
+        }
     }
+    
+    
 
-
+    @IBAction func sharebutton(_ sender: Any) {
+    }
+    
 }
 
